450ccdb89a8a3e27799e0fafd58fcf0d6651f62d
Code Th3ory <codeth3ory@gmail.com>
2013-05-21 08:52:05 -0300

    test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ BOARD_KERNEL_BASE := 0x80000000
 
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := tuna_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/tuna
+TARGET_KERNEL_SOURCE := kernel/samsung/tuna-cog
 
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := omap4
